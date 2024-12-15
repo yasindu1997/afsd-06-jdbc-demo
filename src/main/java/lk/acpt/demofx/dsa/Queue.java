56761a81aec002615ebdb26b1d7c6f12b16f24d1
yasindu1997 <yasindusathsaraoth@gmail.com>
2024-12-15 17:07:29 +0530

    print queue method added

--- src/main/java/lk/acpt/demofx/dsa/Queue.java
+++ src/main/java/lk/acpt/demofx/dsa/Queue.java
@@ -16,4 +16,13 @@ public class Queue {
             temp.next = node;
         }
     }
+
+    public void printQueue(){
+      Node temp = top;
+
+      while(temp!=null){
+          System.out.println(temp.data);
+          temp=temp.next;
+      }
+    }
 }
